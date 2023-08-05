e980c0fb0d43e2b8faa4df2fac3638563c23d7ac
Reyad Khan <khanreyad207@gmail.com>
2023-08-06 00:56:33 +0600

    commit

--- JavaScript.js
+++ JavaScript.js
@@ -1,12 +1,25 @@
-let frindsName = ["REYAD","ONTORA","FAHIM","MULKI","ABDULLHA","SOFUULLHA"];
-frindsName.push("Fatema");
-console.log(frindsName.length);
-console.log(frindsName);
-frindsName.pop();
-frindsName.pop();
-console.log(frindsName.length);
-console.log(frindsName);
+var x = {
+    name: "Reyad",
+    "Full Name": "Reyad Hossain Khan",
+    email: "khanreyad207@gmail.com",
+    myDetail: ["Programmer","Officer"],
+    fabouriteMovies : {
+        bangla: "Suronggo",
+        english: "Tytanic",
+        hindi : "Tarename",
+    }
+};
+x.bookList = { 
+    bangla : "SorotChandra",
+    English : "Haripoter",
+    Arabic : "Arob Rojoni"
+};
 
+Object.freeze(x);
 
-
-
+x.car = { 
+    bangla : "SorotChandra",
+    English : "Haripoter",
+    Arabic : "Arob Rojoni"
+};
+console.log(x);
